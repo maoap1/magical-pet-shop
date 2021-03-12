1b13326dd9d20423ad3c39c1973804e3b107f67d
maoap1 <34171803+maoap1@users.noreply.github.com>
2021-03-12 16:30:50 +0100

    INIT

---
+++ MagicalPetShop/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    private static PlayerState _THIS;
+    public static PlayerState THIS
+    {
+        get
+        {
+            if (_THIS != null) return _THIS;
+            _THIS = GameObject.FindObjectOfType<PlayerState>();
+            if (_THIS == null)
+            {
+                GameObject ps = new GameObject();
+                ps.name = "PlayerState";
+                _THIS = ps.AddComponent<PlayerState>();                
+            }
+            GameObject.DontDestroyOnLoad(_THIS);
+            return _THIS;
+        }
+    }
+   
+    private PlayerState() {}
+
+}
---
+++ MagicalPetShop/Assets/Scripts/UIScaler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UIScaler : MonoBehaviour
+{
+    
+    public int widthUnits = 9;
+    public int heightUnits = 16;
+
+    private float _lastWidth = 0f;
+    private float _lastHeight = 0f;
+
+    void Update()
+    {
+        var parentRect = transform.parent.GetComponent<RectTransform>();
+        float width = parentRect.rect.width;
+        float height = parentRect.rect.height;
+        if ((Mathf.Abs(width - _lastWidth) > 0.1) ||
+            (Mathf.Abs(height - _lastHeight) > 0.1))
+        {
+            Recompute(width, height);
+        }
+    }
+
+    private void Recompute(float width, float height)
+    {
+        var aspectHeight = width / widthUnits * heightUnits;
+        if (Mathf.Abs(aspectHeight - height) < 1f)
+        {
+            SetSize(width, height);
+        }
+        if (aspectHeight < height)
+        {
+            SetSize(width, aspectHeight);
+        }
+        else
+        {
+            var aspectWidth = height / heightUnits * widthUnits;
+            SetSize(aspectWidth, height);
+        }
+    }
+
+    private void SetSize(float width, float height)
+    {
+        var rect = GetComponent<RectTransform>();
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+    }
+}
