ef6afa46f49390aef65d738ec450499cdc9ca010
Michelle123211 <M.stolova@seznam.cz>
2021-03-17 13:29:36 +0100

    Navigation between scenes

---
+++ MagicalPetShop/Assets/Scripts/UI/NavBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavBar : MonoBehaviour {
+
+    [SerializeField]
+    private string location1;
+    [SerializeField]
+    private string scene1;
+    [SerializeField]
+    private Button button1;
+
+    [SerializeField]
+    private string location2;
+    [SerializeField]
+    private string scene2;
+    [SerializeField]
+    private GameObject button2;
+
+    // Start is called before the first frame update
+    void Start() {
+        button1.GetComponent<NavButton>().SetScene(scene1);
+        button1.GetComponentInChildren<Text>().text = location1;
+
+        button2.GetComponent<NavButton>().SetScene(scene2);
+        button2.GetComponentInChildren<Text>().text = location2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
