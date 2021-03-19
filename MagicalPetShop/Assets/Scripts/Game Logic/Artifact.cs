7abc186e0ead2e4cb7a2c9c022645ca5bec610ac
Michelle123211 <M.stolova@seznam.cz>
2021-03-19 02:49:58 +0100

    Overriding (in)equalities

--- MagicalPetShop/Assets/Scripts/Game Logic/Artifact.cs
+++ MagicalPetShop/Assets/Scripts/Game Logic/Artifact.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "Artifact", menuName = "PetShop/Artifact")]
-public class Artifact : ScriptableObject
+public class Artifact : ScriptableObject, IEquatable<Artifact>
 {
     public string name;
     public Sprite artwork;
+
+    public bool Equals(Artifact other) {
+        if (other == null) return false;
+        return this.name == other.name;
+    }
+
+    public override bool Equals(object obj) {
+        if (obj == null) return false;
+
+        Artifact other = obj as Artifact;
+        if (other == null) return false;
+        else return Equals(other);
+    }
+
+    public override int GetHashCode() {
+        return this.name.GetHashCode();
+    }
+
+    public static bool operator ==(Artifact artifact1, Artifact artifact2) {
+        if (((object)artifact1) == null || ((object)artifact2) == null)
+            return object.Equals(artifact1, artifact2);
+        return artifact1.Equals(artifact2);
+    }
+
+    public static bool operator !=(Artifact artifact1, Artifact artifact2) {
+        return !(artifact1 == artifact2);
+    }
 }
--- MagicalPetShop/Assets/Scripts/Game Logic/Essence.cs
+++ MagicalPetShop/Assets/Scripts/Game Logic/Essence.cs
@@ -1,9 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "Essence", menuName = "PetShop/Essence")]
-public class Essence : ScriptableObject
+public class Essence : ScriptableObject, IEquatable<Essence>
 {
     public (string, int) essenceCount;
+
+    public bool Equals(Essence other) {
+        if (other == null) return false;
+        return this.essenceCount.Item1 == other.essenceCount.Item1;
+    }
+
+    public override bool Equals(object obj) {
+        if (obj == null) return false;
+
+        Essence other = obj as Essence;
+        if (other == null) return false;
+        else return Equals(other);
+    }
+
+    public override int GetHashCode() {
+        return this.essenceCount.Item1.GetHashCode();
+    }
+
+    public static bool operator ==(Essence essence1, Essence essence2) {
+        if (((object)essence1) == null || ((object)essence2) == null)
+            return object.Equals(essence1, essence2);
+        return essence1.Equals(essence2);
+    }
+
+    public static bool operator !=(Essence essence1, Essence essence2) {
+        return !(essence1 == essence2);
+    }
 }
