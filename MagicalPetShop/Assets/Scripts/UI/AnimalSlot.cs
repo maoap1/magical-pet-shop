3f74025ec655b3b988f1cdfda7fc91fc0865d4d1
koblizekjan <koblizekjan@gmail.com>
2021-04-28 10:57:34 +0200

    Update AnimalSlot.cs

--- MagicalPetShop/Assets/Scripts/UI/AnimalSlot.cs
+++ MagicalPetShop/Assets/Scripts/UI/AnimalSlot.cs
@@ -14,10 +14,16 @@ public class AnimalSlot : MonoBehaviour
     public Image image;
     [SerializeField]
     private Text count;
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> parent of 24a2026 (Revert "fixed some issues")
     [SerializeField]
     private Text quality;
     [SerializeField]
     private Button info;
+<<<<<<< HEAD
 
     private InventoryUI inventory;
     private RecipeProgress recipeProgress;
@@ -28,6 +34,24 @@ public class AnimalSlot : MonoBehaviour
         this.value.text = ((int)(animal.animal.value * GameLogic.THIS.getRarityMultiplier(animal.rarity) * PlayerState.THIS.recipes.Find(r => r.animal == animal.animal).costMultiplier)).ToString();
         this.image.sprite = animal.animal.artwork;
         this.count.text = animal.count.ToString();
+=======
+
+    private InventoryUI inventory;
+    private RecipeProgress recipeProgress;
+>>>>>>> Stashed changes
+
+    public void SetAnimal(InventoryAnimal animal, InventoryUI inventory) {
+        this.inventory = inventory;
+        this.name.text = animal.animal.name;
+<<<<<<< Updated upstream
+        this.value.text = animal.animal.value.ToString();
+        this.image.sprite = animal.animal.artwork;
+        this.count.text = animal.count.ToString();
+=======
+        this.value.text = ((int)(animal.animal.value * GameLogic.THIS.getRarityMultiplier(animal.rarity) * PlayerState.THIS.recipes.Find(r => r.animal == animal.animal).costMultiplier)).ToString();
+        this.image.sprite = animal.animal.artwork;
+        this.count.text = animal.count.ToString();
+>>>>>>> parent of 24a2026 (Revert "fixed some issues")
         this.quality.text = animal.rarity.ToString("G");
         this.recipeProgress = PlayerState.THIS.recipes.Find(r => r.recipe.animal == animal.animal);
     }
@@ -38,5 +62,9 @@ public class AnimalSlot : MonoBehaviour
         {
             inventory.recipeInfo.Open(recipeProgress);
         }
+<<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
+>>>>>>> parent of 24a2026 (Revert "fixed some issues")
     }
 }
