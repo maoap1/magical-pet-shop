29c7ae7dba5c3efa767887b0f70314dbe46b477e
Michelle123211 <M.stolova@seznam.cz>
2021-05-11 19:27:08 +0200

    CustomerDisplay2 updated

--- MagicalPetShop/Assets/Scripts/UI/CustomerDisplay2.cs
+++ MagicalPetShop/Assets/Scripts/UI/CustomerDisplay2.cs
@@ -10,7 +10,8 @@ public class CustomerDisplay2 : MonoBehaviour {
     public Image order;
     public OrderPanel orderPanel;
 
-    private bool justLoaded = false;
+    private bool hidden;
+    private bool justLoaded;
 
     public void CustomerClicked() {
         if (Shop.customers[CustomerID].hasValue) {
@@ -18,28 +19,29 @@ public class CustomerDisplay2 : MonoBehaviour {
         }
     }
 
-    private void OnMouseDown() {
-        if (!Utils.IsPointerOverGameObject() && Shop.customers[CustomerID].hasValue) {
-            orderPanel.Open(Shop.customers[CustomerID]);
-        }
-    }
-
     private void Start() {
+        this.hidden = true;
         this.justLoaded = true;
     }
 
     private void Update() {
         if (Shop.customers != null && (Shop.customers[CustomerID] == null || !Shop.customers[CustomerID].hasValue)) {
             character.SetActive(false);
-        } else if (Shop.customers != null && Shop.customers[CustomerID].hasValue) {
+            this.hidden = true;
+            if (Shop.customers != null && Shop.customers[CustomerID] != null && !Shop.customers[CustomerID].hasValue)
+                this.justLoaded = false;
+        } else if (Shop.customers != null && Shop.customers[CustomerID].hasValue && this.hidden) {
             InventoryAnimal desiredAnimal = Shop.customers[CustomerID].desiredAnimal;
             order.sprite = desiredAnimal.animal.artwork;
             order.material = new Material(order.material);
             order.material.SetColor("_Color", GameGraphics.THIS.getRarityColor(desiredAnimal.rarity));
             order.material.SetTexture("_BloomTex", desiredAnimal.animal.bloomSprite.texture);
-            if (!character.activeInHierarchy & !this.justLoaded) FindObjectOfType<AudioManager>().Play(SoundType.CustomerAppear);
             character.SetActive(true);
+            if (!this.justLoaded) {
+                FindObjectOfType<AudioManager>().Play(SoundType.CustomerAppear);
+            }
+            this.hidden = false;
+            this.justLoaded = false;
         }
-        this.justLoaded = false;
     }
 }
