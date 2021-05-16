6569966d42d62a41a7ec65d591ec5b92ae157151
Michelle123211 <M.stolova@seznam.cz>
2021-05-16 10:32:46 +0200

    Background blur behind small overlays

--- MagicalPetShop/Assets/Crafting/NewRecipeDisplay.cs
+++ MagicalPetShop/Assets/Crafting/NewRecipeDisplay.cs
@@ -9,22 +9,37 @@ public class NewRecipeDisplay : MonoBehaviour
     public TMPro.TextMeshProUGUI animalName;
     private bool level = false;
     private NewLevelDisplay newLevelDisplay;
+
+    public List<GameObject> objectsToAppear;
+    public List<GameObject> objectsToHide;
+
     public void Open(RecipeProgress rp, bool newLevel)
     {
         level = newLevel;
         animalImage.sprite = rp.animal.artwork;
         animalName.text = rp.animal.name;
+        foreach (GameObject g in objectsToAppear) {
+            g.SetActive(true);
+        }
         this.gameObject.SetActive(true);
+        foreach (GameObject g in objectsToHide) {
+            g.SetActive(false);
+        }
         FindObjectOfType<AudioManager>().Play(SoundType.Success);
         newLevelDisplay = Resources.FindObjectsOfTypeAll<NewLevelDisplay>()[0];
     }
 
-    public void Close()
-    {
+    public void Close() {
+        this.gameObject.SetActive(false);
+        foreach (GameObject g in objectsToAppear) {
+            g.SetActive(false);
+        }
+        foreach (GameObject g in objectsToHide) {
+            g.SetActive(true);
+        }
         if (level)
         {
             newLevelDisplay.Open();
         }
-        this.gameObject.SetActive(false);
     }
 }
