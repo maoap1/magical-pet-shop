5b677deddd32503ddcf6837eadd2317cf9218fe3
maoap1 <34171803+maoap1@users.noreply.github.com>
2021-05-16 20:53:02 +0200

    Tweenable Bugfix

--- MagicalPetShop/Assets/Scripts/Tweens/Tweenable.cs
+++ MagicalPetShop/Assets/Scripts/Tweens/Tweenable.cs
@@ -6,15 +6,28 @@ public class Tweenable : MonoBehaviour
     public UnityEvent OnDisableTween = null;
     public UnityEvent OnEnableTween = null;
 
+    private bool IsNull(UnityEvent unityEvent)
+    {
+        if (unityEvent == null) return true;
+        for (int i = 0; i < unityEvent.GetPersistentEventCount(); i++)
+        {
+            if (unityEvent.GetPersistentTarget(i) != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void Disable()
     {
-        if (OnDisableTween == null) gameObject.SetActive(false);
-        else OnDisableTween.Invoke();
+        if (IsNull(OnDisableTween)) { gameObject.SetActive(false); Debug.Log("Disable null"); }
+        else { OnDisableTween.Invoke(); Debug.Log("Disable tween"); }
     }
 
     public void Enable()
     {
-        if (OnEnableTween == null) gameObject.SetActive(true);
-        else OnEnableTween.Invoke();
+        if (IsNull(OnEnableTween)) { gameObject.SetActive(true); Debug.Log("Enable null"); }
+        else { OnEnableTween.Invoke(); Debug.Log("Enable tween"); }
     }
 }
