aa51fd5b533823ab4080c934dcb69a9bd09150d6
Michelle123211 <M.stolova@seznam.cz>
2021-05-28 00:15:46 +0200

    Cauldron and essences shrinking

--- MagicalPetShop/Assets/UI Utils/Scripts/ButtonShrink.cs
+++ MagicalPetShop/Assets/UI Utils/Scripts/ButtonShrink.cs
@@ -9,21 +9,26 @@ using DG.Tweening;
 public class ButtonShrink : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
     public GameObject target = null;
+    public float targetScalePercent = 0.95f;
+    private float originalScaleX;
+    private float originalScaleY;
 
     public void Start()
     {
         if (!target) target = gameObject;
+        originalScaleX = target.transform.localScale.x;
+        originalScaleY = target.transform.localScale.y;
     }
 
     public void OnPointerDown(PointerEventData eventData) {
         Button button = GetComponent<Button>();
         if (button == null || (button != null && button.interactable)) {
-            target.transform.DOScale(new Vector3(0.95f, 0.95f, 0.95f), 0.1f);
+            target.transform.DOScale(new Vector3(targetScalePercent * originalScaleX, targetScalePercent * originalScaleY, 1f), 0.1f);
         }
     }
 
     public void OnPointerUp(PointerEventData eventData) {
         if (target != null)
-            target.transform.DOScale(new Vector3(1f, 1f, 1f), 0.1f);
+            target.transform.DOScale(new Vector3(originalScaleX, originalScaleY, 1f), 0.1f);
     }
 }
